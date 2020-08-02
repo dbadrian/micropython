7ac7d5a29854a2f18bd5b2cb814689cb96b857ad
David B. Adrian <dawidh.adrian@googlemail.com>
2020-08-02 16:42:09 +0200

    Removing unneed code fragments to save memory

--- ports/esp8266/boards/manifest_release.py
+++ ports/esp8266/boards/manifest_release.py
@@ -1,7 +1,7 @@
 include("manifest.py")
 
 # drivers
-freeze("$(MPY_DIR)/drivers/display", "ssd1306.py")
+# freeze("$(MPY_DIR)/drivers/display", "ssd1306.py")
 
 # file utilities
 freeze("$(MPY_LIB_DIR)/upysh", "upysh.py")
@@ -11,13 +11,13 @@ freeze("$(MPY_LIB_DIR)/urequests", "urequests.py")
 freeze("$(MPY_LIB_DIR)/urllib.urequest", "urllib/urequest.py")
 
 # umqtt with examples
-freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")
-freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")
-freeze("$(MPY_LIB_DIR)/umqtt.simple", "example_pub_button.py")
-freeze("$(MPY_LIB_DIR)/umqtt.simple", "example_sub_led.py")
+# freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")
+# freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")
+# freeze("$(MPY_LIB_DIR)/umqtt.simple", "example_pub_button.py")
+# freeze("$(MPY_LIB_DIR)/umqtt.simple", "example_sub_led.py")
 
 # HTTP examples
-freeze("$(MPY_DIR)/examples/network", "http_client.py")
-freeze("$(MPY_DIR)/examples/network", "http_client_ssl.py")
-freeze("$(MPY_DIR)/examples/network", "http_server.py")
-freeze("$(MPY_DIR)/examples/network", "http_server_ssl.py")
+# freeze("$(MPY_DIR)/examples/network", "http_client.py")
+# freeze("$(MPY_DIR)/examples/network", "http_client_ssl.py")
+# freeze("$(MPY_DIR)/examples/network", "http_server.py")
+# freeze("$(MPY_DIR)/examples/network", "http_server_ssl.py")
